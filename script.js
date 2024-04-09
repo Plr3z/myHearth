96185181e212a79f5621ddf0f4ca06cb46ee30c0
Plr3z <plrezende02@gmail.com>
2024-04-09 17:08:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade');
+
+
+function abrirModal(){
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+}
+
+function fecharModal(){
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+}
